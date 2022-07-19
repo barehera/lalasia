c1ebb2483f40e2e820c46a3cec9e05a5840180c5
Cagan Buyukavcilar <mircagi@hotmail.com>
2022-07-20 01:06:05 +0300

    mobile navbar added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,84 +1,151 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/logo.svg";
 import userIcon from "../assets/icons/user.svg";
 import bagIcon from "../assets/icons/bag.svg";
 import menuIcon from "../assets/icons/menu.svg";
 import { Link, useLocation } from "react-router-dom";
-
+import { AiOutlineClose } from "react-icons/ai";
 const Header = () => {
   const location = useLocation();
-
+  const [open, setOpen] = useState(false);
   return (
-    <header className="px-6 lg:px-24 flex items-center justify-between text-text shadow-md h-[72px] lg:h-[120px] ">
-      <Link to="/">
-        <div className="flex gap-3 items-center justify-center cursor-pointer hover:scale-110 transition-all duration-500 ease-out">
-          <img src={logo} alt="Logo" className="h-11 w-11" />
-          <h2 className="text-lg font-bold">Lalasia</h2>
-        </div>
-      </Link>
+    <>
+      <header className="px-6 lg:px-24 flex items-center justify-between text-text shadow-md h-[72px] lg:h-[120px] ">
+        <Link to="/">
+          <div className="flex gap-3 items-center justify-center cursor-pointer hover:scale-110 transition-all duration-500 ease-out">
+            <img src={logo} alt="Logo" className="h-11 w-11" />
+            <h2 className="text-lg font-bold">Lalasia</h2>
+          </div>
+        </Link>
 
-      <nav className="lg:flex items-center justify-center hidden h-full">
-        <ul className="flex items-center justify-center gap-2 xl:gap-10 h-full">
-          <li className="h-full">
-            <Link
+        <nav className="lg:flex items-center justify-center hidden h-full">
+          <ul className="flex items-center justify-center gap-2 xl:gap-10 h-full">
+            <li className="h-full">
+              <Link
+                className={`${
+                  location.pathname === "/product"
+                    ? " border-main text-main  hover:border-secondary"
+                    : " border-transparent text-text"
+                }  cursor-pointer border-b-4 flex items-center justify-center h-full  text-lg hover:-skew-x-12 px-5 hover:bg-main hover:text-white   transition-all duration-500 ease-out `}
+                to="/product"
+              >
+                Product
+              </Link>
+            </li>
+            <li
               className={`${
-                location.pathname === "/product"
+                location.pathname === "/services"
                   ? " border-main text-main  hover:border-secondary"
                   : " border-transparent text-text"
-              }  cursor-pointer border-b-4 flex items-center justify-center h-full  text-lg hover:-skew-x-12 px-5 hover:bg-main hover:text-white   transition-all duration-500 ease-out `}
-              to="/product"
+              }  cursor-pointer border-b-4 flex items-center justify-center h-full  text-lg hover:-skew-x-12 px-5 hover:bg-main hover:text-white   transition-all duration-500 ease-out`}
             >
-              Product
-            </Link>
-          </li>
-          <li
-            className={`${
-              location.pathname === "/services"
-                ? " border-main text-main  hover:border-secondary"
-                : " border-transparent text-text"
-            }  cursor-pointer border-b-4 flex items-center justify-center h-full  text-lg hover:-skew-x-12 px-5 hover:bg-main hover:text-white   transition-all duration-500 ease-out`}
-          >
-            Services
-          </li>
-          <li
-            className={`${
-              location.pathname === "/article"
-                ? " border-main text-main  hover:border-secondary"
-                : " border-transparent text-text"
-            }  cursor-pointer border-b-4 flex items-center justify-center h-full  text-lg hover:-skew-x-12 px-5 hover:bg-main hover:text-white   transition-all duration-500 ease-out`}
-          >
-            Article
-          </li>
-          <li className="h-full">
-            <Link
+              Services
+            </li>
+            <li
               className={`${
-                location.pathname === "/about-us"
+                location.pathname === "/article"
                   ? " border-main text-main  hover:border-secondary"
                   : " border-transparent text-text"
-              }  cursor-pointer border-b-4 flex items-center justify-center h-full  text-lg hover:-skew-x-12 px-5 hover:bg-main hover:text-white   transition-all duration-500 ease-out `}
-              to="/about-us"
+              }  cursor-pointer border-b-4 flex items-center justify-center h-full  text-lg hover:-skew-x-12 px-5 hover:bg-main hover:text-white   transition-all duration-500 ease-out`}
             >
-              About Us
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <nav className="lg:flex items-center justify-center hidden">
-        <ul className="flex items-center justify-center gap-6">
-          <li className="cursor-pointer">
-            <img src={bagIcon} alt="Shopping Bag Icon"></img>
-          </li>
-          <li className="cursor-pointer">
-            <img src={userIcon} alt="User Icon"></img>
-          </li>
-        </ul>
-      </nav>
-      <img
-        src={menuIcon}
-        alt="Menu Icon"
-        className="lg:hidden cursor-pointer"
-      />
-    </header>
+              Article
+            </li>
+            <li className="h-full">
+              <Link
+                className={`${
+                  location.pathname === "/about-us"
+                    ? " border-main text-main  hover:border-secondary"
+                    : " border-transparent text-text"
+                }  cursor-pointer border-b-4 flex items-center justify-center h-full  text-lg hover:-skew-x-12 px-5 hover:bg-main hover:text-white   transition-all duration-500 ease-out `}
+                to="/about-us"
+              >
+                About Us
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <nav className="lg:flex items-center justify-center hidden">
+          <ul className="flex items-center justify-center gap-6">
+            <li className="cursor-pointer">
+              <img src={bagIcon} alt="Shopping Bag Icon"></img>
+            </li>
+            <li className="cursor-pointer">
+              <img src={userIcon} alt="User Icon"></img>
+            </li>
+          </ul>
+        </nav>
+        {open ? (
+          <>
+            <AiOutlineClose
+              onClick={() => setOpen(false)}
+              className="text-2xl cursor-pointer lg:hidden"
+            ></AiOutlineClose>
+            <img
+              src={menuIcon}
+              alt="Menu Icon"
+              onClick={() => setOpen(true)}
+              className=" cursor-pointer w-8 hidden lg:block"
+            />
+          </>
+        ) : (
+          <img
+            src={menuIcon}
+            alt="Menu Icon"
+            onClick={() => setOpen(true)}
+            className="lg:hidden cursor-pointer w-8"
+          />
+        )}
+      </header>
+      {/*Mobile nav */}
+      {open && (
+        <nav className="  bg-white h-[72px]  items-center justify-center border-y lg:hidden">
+          <ul className="flex items-center justify-center xl:gap-10 h-full w-full">
+            <li className="h-full w-full">
+              <Link
+                className={`${
+                  location.pathname === "/product"
+                    ? " bg-main text-white font-bold"
+                    : " border-transparent text-black font-semibold"
+                }  cursor-pointer  flex items-center justify-center h-full  text-sm px-3 hover:bg-main hover:text-white   transition-all duration-500 ease-out `}
+                to="/product"
+              >
+                Product
+              </Link>
+            </li>
+            <li
+              className={`${
+                location.pathname === "/services"
+                  ? " bg-main text-white font-bold"
+                  : " border-transparent text-black font-semibold"
+              }  cursor-pointer  flex items-center justify-center h-full w-full  text-sm px-3 hover:bg-main hover:text-white   transition-all duration-500 ease-out`}
+            >
+              Services
+            </li>
+            <li
+              className={`${
+                location.pathname === "/article"
+                  ? " bg-main text-white font-bold"
+                  : " border-transparent text-black font-semibold"
+              }  cursor-pointer  flex items-center justify-center h-full w-full  text-sm px-3 hover:bg-main hover:text-white   transition-all duration-500 ease-out`}
+            >
+              Article
+            </li>
+            <li className="h-full w-full">
+              <Link
+                className={`${
+                  location.pathname === "/about-us"
+                    ? " bg-main text-white font-bold"
+                    : " border-transparent text-black font-semibold"
+                }  cursor-pointer  flex items-center justify-center h-full  text-sm px-3 hover:bg-main hover:text-white   transition-all duration-500 ease-out `}
+                to="/about-us"
+              >
+                About Us
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </>
   );
 };
 
